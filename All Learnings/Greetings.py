579ca536961cbaaa6bba06a4a0c25a845144e1c5
RonikChatterjee <ronikchatterjee615@gmail.com>
2024-05-25 18:28:19 +0530

    implementation of time module

--- All Learnings/Greetings.py
+++ All Learnings/Greetings.py
@@ -1,1 +1,12 @@
-
+import time
+timestamp = int(time.strftime("%H"))
+#print(timestamp)
+name = input("Enter your name : ")
+if(timestamp < 12):
+    print("Good Morning", name)
+elif(timestamp >= 12 and timestamp < 16):
+    print("Good Afternoon", name)
+elif(timestamp >= 16 and timestamp < 20):
+    print("Good Evening", name)
+else:
+    print("Good Night", name)
